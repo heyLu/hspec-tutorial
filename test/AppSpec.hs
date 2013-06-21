f8bda29d6a226395d488d8449a4ba6a20e8d5a7c
Lucas Stadler <mi.au@papill0n.org>
2013-06-21 10:14:06 +0200

    Pass test for simple arbitrary paths.

--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -4,7 +4,7 @@ module AppSpec (main, spec) where
 import           Test.Hspec
 import           Test.QuickCheck
 import           Network.Wai.Test (SResponse)
-import           Data.ByteString (ByteString)
+import           Data.ByteString (ByteString, pack)
 import           Control.Applicative
 
 import           Helper
@@ -15,6 +15,9 @@ main = hspec spec
 
 get :: ByteString -> IO SResponse
 get path = app >>= getPath path
+
+instance Arbitrary ByteString where
+  arbitrary = fmap pack arbitrary
 
 spec :: Spec
 spec = do
@@ -31,5 +34,5 @@ spec = do
 
   context "when given an *arbitrary* invalid request path" $ do
     it "responds with HTTP status 404" $ do
-      property $
-        pending
+      property $ \s -> s `notElem` ["", "/"] ==>
+        get s `shouldRespondWith` 404
