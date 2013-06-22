15e3bc8ddddfd4347dba294bc0ee295fe9bad20a
Lucas Stadler <mi.au@papill0n.org>
2013-06-22 17:40:40 +0200

    Use a type for the version.

--- src/App.hs
+++ src/App.hs
@@ -5,6 +5,7 @@ import           Data.Aeson hiding (json)
 import           GHC.Generics
 import           Web.Scotty hiding (body)
 import           Network.Wai
+import           Data.List (intercalate)
 
 data Message = Message {
   body :: String
@@ -13,11 +14,23 @@ data Message = Message {
 instance ToJSON Message
 instance FromJSON Message
 
+data Version = Version {
+    name :: String,
+    version :: (Int, Int, Int)
+}
+
+showVersion (maj, min, patch) =
+    intercalate "." $ map show [maj, min, patch]
+
+instance ToJSON Version where
+    toJSON (Version name version) =
+        object [
+            "name" .= name,
+            "version" .= showVersion version
+        ]
+
 app :: IO Application
 app = scottyApp $ do
-  get "" $ json $ object [
-      "name" .= ("time-service" :: String),
-      "version" .= ("0.1.0" :: String)
-    ]
+  get "" $ json (Version "time-service" (0, 1, 0))
   get "/hello" $ do
     json (Message "Hello!")
