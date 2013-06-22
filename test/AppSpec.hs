190d9e59035dac34ca7e8580c6b2a58cff7782bf
Lucas Stadler <mi.au@papill0n.org>
2013-06-22 16:12:44 +0200

    Add arbitrary instance for simple paths.
    These are hopefully similar to those returned by `Network.Wai.pathInfo`,
    e.g. without a query part.
    
    Using that instance we can test for arbitrary paths not being accepted
    and given a 404 error while still accepting those paths that we handle.

--- test/AppSpec.hs
+++ test/AppSpec.hs
@@ -4,7 +4,7 @@ module AppSpec (main, spec) where
 import           Test.Hspec
 import           Test.QuickCheck
 import           Network.Wai.Test (SResponse)
-import           Data.ByteString (ByteString, pack)
+import qualified Data.ByteString.Char8 as B
 import           Control.Applicative
 
 import           Helper
@@ -13,11 +13,19 @@ import           App
 main :: IO ()
 main = hspec spec
 
-get :: ByteString -> IO SResponse
+get :: B.ByteString -> IO SResponse
 get path = app >>= getPath path
 
-instance Arbitrary ByteString where
-  arbitrary = fmap pack arbitrary
+newtype Path = Path B.ByteString deriving Show
+instance Arbitrary Path where
+    arbitrary = fmap (Path . B.pack) $ listOf . oneof $ [choose ('a', 'z'), choose ('0', '9'), return '/']
+
+isNoPathIn p ps = normalizePath p `notElem` allowedPaths
+    where allowedPaths = concatMap (\path -> [path, B.snoc path '/']) ps
+normalizePath = fst . B.foldl step ("", False)
+    where step (p, True)  '/' = (p, True)
+          step (p, False) '/' = (B.snoc p '/', True)
+          step (p, _)     c   = (B.snoc p c, False)
 
 spec :: Spec
 spec = do
@@ -34,5 +42,5 @@ spec = do
 
   context "when given an *arbitrary* invalid request path" $ do
     it "responds with HTTP status 404" $ do
-      property $ \s -> s `notElem` ["", "/"] ==>
-        get s `shouldRespondWith` 404
+      property $ \(Path p) -> p `isNoPathIn` [""] ==>
+        get (p) `shouldRespondWith` 404
